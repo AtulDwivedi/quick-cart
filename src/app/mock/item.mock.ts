188b1e88136d5445e1625d47f4531a216c0ec1e0
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-06-16 01:10:10 +0530

    added Items page with bootstrap changes

--- src/app/mock/item.mock.ts
+++ src/app/mock/item.mock.ts
@@ -40,5 +40,53 @@ const ITEMS: Item[] = [
         brand: "Google",
         imageUrl: "assets/images/item/1002.png",
         otherImagesUrl: ["assets/images/item/1002.png"]
-    }
+    },
+    {
+        id: "ITM_1001",
+        name: "Item 1001",
+        description: "Item 1001 Description",
+        price: 1001.10,
+        quantity: 1001,
+        creationDate: new Date("Dec 21, 1965"),
+        expiryDate: new Date("Dec 21, 1966"),
+        brand: "Apple",
+        imageUrl: "assets/images/item/1001.png",
+        otherImagesUrl: ["assets/images/item/1001.png"]
+    },
+    {
+        id: "ITM_1002",
+        name: "Item 1002",
+        description: "Item 1002 Description",
+        price: 1001.10,
+        quantity: 1001,
+        creationDate: new Date("Dec 21, 1975"),
+        expiryDate: new Date("Dec 21, 1976"),
+        brand: "Google",
+        imageUrl: "assets/images/item/1002.png",
+        otherImagesUrl: ["assets/images/item/1002.png"]
+    },
+    {
+        id: "ITM_1001",
+        name: "Item 1001",
+        description: "Item 1001 Description",
+        price: 1001.10,
+        quantity: 1001,
+        creationDate: new Date("Dec 21, 1965"),
+        expiryDate: new Date("Dec 21, 1966"),
+        brand: "Apple",
+        imageUrl: "assets/images/item/1001.png",
+        otherImagesUrl: ["assets/images/item/1001.png"]
+    },
+    {
+        id: "ITM_1002",
+        name: "Item 1002",
+        description: "Item 1002 Description",
+        price: 1001.10,
+        quantity: 1001,
+        creationDate: new Date("Dec 21, 1975"),
+        expiryDate: new Date("Dec 21, 1976"),
+        brand: "Google",
+        imageUrl: "assets/images/item/1002.png",
+        otherImagesUrl: ["assets/images/item/1002.png"]
+    }   
 ]
